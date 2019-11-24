a7ecb77d43565e2e8dfdc52b16ac29d7ecc68e1e
Jimmy Girardet <ijkl@netc.fr>
2019-11-24 12:58:32 +0100

    build cscript

---
+++ scripted/build_executable.py
@@ -0,0 +1,48 @@
+#:/usr/bin/python
+
+import subprocess
+import platform
+from pathlib import Path
+import PyInstaller.__main__
+import shutil
+
+import logging
+
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
+
+WORKING_DIR = Path.cwd()
+LOG.info(" working dir: %s", WORKING_DIR)
+
+SPEC_NAME = "main_win.spec" if platform.system() == "Windows" else "main.spec"
+SPEC_PATH = WORKING_DIR / SPEC_NAME
+assert SPEC_PATH.is_file()
+LOG.info("spec_path: %s", str(SPEC_PATH))
+
+
+EXT = ".exe" if platform.system() == "Windows" else ""
+BIN_NAME = "MyDevoirs" + EXT
+DIST_PATH = WORKING_DIR / "dist"
+BUILD_PATH = WORKING_DIR / "build"
+BIN_PATH = (DIST_PATH/ BIN_NAME).absolute()
+LOG.info('bin_path: %s', str(BIN_PATH))
+
+# cleanup previous
+for rep in [DIST_PATH, BUILD_PATH]:
+    if rep.is_dir():
+        LOG.info('removing :%s', str(rep))
+        shutil.rmtree(str(rep))
+
+
+# build
+PyInstaller.__main__.run([
+    '-y',
+    '--clean',
+    str(SPEC_PATH),
+])
+
+# test exec
+try:
+    proc = subprocess.run(str(BIN_PATH), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+except subprocess.CalledProcessError as err:
+    LOG.error("Failed running with code %s with following output \n %s", err.returncode,err.output.decode() )
