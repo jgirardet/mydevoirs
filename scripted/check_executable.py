edaf458c8430d4b82fe7724da19c0eff4dd0cb59
jgirardet <ijkl@netc.fr>
2019-12-10 20:36:37 +0100

    azdazd

--- scripted/check_executable.py
+++ scripted/check_executable.py
@@ -71,7 +71,7 @@ def run_mydevoirs():
 
 
 if __name__ == "__main__":
-    LOG.info(platform.system())
+    # LOG.info(platform.system())
     LOG.info("avant logDD")
     # LOG.info("ddb %s", str(DDB))
     # LOG.info("ddb exists %s", DDB.exists())
