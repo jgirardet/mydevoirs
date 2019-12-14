136b6808702a677412d3f5cfb01f54e2d2cbfc55
jgirardet <ijkl@netc.fr>
2019-12-14 23:20:28 +0100

    change focus

--- mydevoirs/ouinonpopup.py
+++ mydevoirs/ouinonpopup.py
@@ -5,7 +5,7 @@ from kivy.properties import BooleanProperty, ObjectProperty
 from kivy.lang import Builder
 
 KV = """
-<OuiNonButton@ToggleButton>:
+<OuiNonButton@Button>:
     bold: True
 
 <OuiNonPopupButtons>:
@@ -25,81 +25,75 @@ KV = """
 
 Builder.load_string(KV)
 
+
 class OuiNonPopup(Popup):
 
-    auto_open=BooleanProperty(True)
+    auto_open = BooleanProperty(True)
 
-    __events__ = ('on_oui', 'on_non') #', 'on_pre_dismiss', 'on_dismiss')
+    __events__ = ("on_oui", "on_non")  #', 'on_pre_dismiss', 'on_dismiss')
 
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         # self.register_event_type("on_oui")
         super().__init__(*args, **kwargs)
         self.content = OuiNonPopupButtons(popup=self)
         if self.auto_open:
             self.open()
 
-
-    def on_oui(self,*args, **kwargs):
-        print('onoui', args, kwargs)
-    # def on_oui(self,*args):
-    #     print("base on_oui func")
+    def on_oui(self, *args, **kwargs):
         self.dismiss()
 
-    def on_non(self,*args):
-        print("on_non")
+    def on_non(self, *args,**kwargs):
         self.dismiss()
 
-from functools import partial
 
 class OuiNonPopupButtons(FocusBehavior, BoxLayout):
     popup = ObjectProperty()
 
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ids.oui.bind(on_press = self._on_press_oui)
-        self.ids.non.bind(on_press = self._on_press_non)
-        # self.ids.oui.bind(on_press = partial(self.popup.dispatch, "on_oui", *args, **kwargs))
-        # self.ids.oui.bind(on_press = self.popup.on_non)
+        self.ids.oui.bind(on_press=self._on_press_oui)
+        self.ids.non.bind(on_press=self._on_press_non)
         self.focus = True
 
-    def _on_press_oui(self,*args):
-        self.popup.dispatch('on_oui')
+    def _on_press_oui(self, *args):
+        self.ids.oui.state = "down"
+        self.ids.non.state = "normal"
+        self.popup.dispatch("on_oui")
 
-    def _on_press_non(self,*args):
-        self.popup.dispatch('on_non')
+    def _on_press_non(self, *args):
+        self.ids.oui.state = "normal"
+        self.ids.non.state = "down"
+        self.popup.dispatch("on_non")
+
+    def _toggle_state(self):
+        backup = self.ids.oui.state
+        self.ids.oui.state = self.ids.non.state
+        self.ids.non.state = backup
 
     def keyboard_on_key_down(self, window, keycode, text, modifier):
         if keycode[1] in ["left", "right"]:
-            backup = self.ids.oui.state
-            self.ids.oui.state = self.ids.non.state
-            self.ids.non.state = backup
+            self._toggle_state()
 
         elif keycode[1] == "enter":
             if self.ids.oui.state == "down":
-                self.popup.dispatch('on_oui')
+                self.popup.dispatch("on_oui")
             else:
-                self.popup.dispatch('on_non')
+                self.popup.dispatch("on_non")
         else:
             return False
         return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from kivy.app import App
     from kivy.uix.widget import Widget
 
     class SettingsApp(App):
-
         def build(self):
             s = Widget()
             s.bind(on_close=self.stop)
-            p = OuiNonPopup(title="mokfzmofkfzemok pkm", on_oui=self.bla)
-            # p.bind(on_oui=self.bla)
-            # p.open()
+            p = OuiNonPopup(title="mokfzmofkfzemok pkm", on_oui=self.stop)
             return s
 
-        def bla(self,*args,**kwargs):
-            print('bla', args, kwargs)
 
     SettingsApp().run()
