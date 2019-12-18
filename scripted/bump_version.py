69c381fbfd4a6870f71cce0d0e8386c31fd9cdad
jgirardet <ijkl@netc.fr>
2019-12-18 15:56:55 +0100

    0.6.0

--- scripted/bump_version.py
+++ scripted/bump_version.py
@@ -5,9 +5,10 @@ from pathlib import Path
 
 GET_TAG = "git describe --tags $(git rev-list --tags --max-count=1)"
 MODE = ["patch", "minor", "major", "prepatch", "preminor", "premajor", "prerelease"]
-README = Path('README.md')
+README = Path("README.md")
 
 assert README.exists()
+
 
 def get_tag():
     tag = subprocess.check_output(GET_TAG, shell=True).strip()
@@ -18,10 +19,15 @@ def get_poestry_version():
     version = toml.loads(open("pyproject.toml").read())["tool"]["poetry"]["version"]
     return version
 
+
 def update_doc_link(previous, new_version):
-    readme  = README.read_text()
-    new = readme.replace(f"https://github.com/jgirardet/mydevoirs/releases/download/{previous}/", f"https://github.com/jgirardet/mydevoirs/releases/download/{new_version}/")
+    readme = README.read_text()
+    new = readme.replace(
+        f"https://github.com/jgirardet/mydevoirs/releases/download/{previous}/",
+        f"https://github.com/jgirardet/mydevoirs/releases/download/{new_version}/",
+    )
     README.write_text(new)
+
 
 if __name__ == "__main__":
     version = get_poestry_version()
@@ -35,14 +41,16 @@ if __name__ == "__main__":
     if mode not in MODE:
         raise KeyError(f"argument {sys.argv[-1]} doit être un de {MODE}")
 
-    new_version  = subprocess.check_output(f"poetry version {mode}", shell=True).strip().split()[-1].decode()
+    new_version = (
+        subprocess.check_output(f"poetry version {mode}", shell=True)
+        .strip()
+        .split()[-1]
+        .decode()
+    )
 
-    update_doc_link(version , new_version)
-    
+    update_doc_link(version, new_version)
+
     subprocess.check_output(f"git commit -a -m {new_version}", shell=True)
     subprocess.check_output(f"git tag {new_version}", shell=True)
 
     assert get_tag() == get_poestry_version()
-
-
-
