d2ea86228a652e08a4641876bf567f4576473df9
Jimmy Girardet <ijkl@netc.fr>
2020-07-22 09:03:16 +0200

    add  	libfontconfig1

--- tests/test_avertissement.py
+++ tests/test_avertissement.py
@@ -7,16 +7,38 @@ from mydevoirs.avertissement import BackupAncienneDB
 from .fixtures import *
 
 
-class MockApp(App):
+class MockApp(MagicMock):
+    # stop = MagicMock()
     pass
 
 
-@patch("mydevoirs.avertissement.App.get_running_App.return_value", MockApp())
+@patch("mydevoirs.avertissement.App.get_running_app", MockApp)
 class TestBackupancienneDB(MyDevoirsTestCase):
     def test_init(self):
         self.check_super_init("Widget", BackupAncienneDB, "old", "new")
 
-    def test_popup_base(self):
+    def test_base(self):
         a = BackupAncienneDB("old", "new")
-        a.old_path == "old"
-        a.backup_path == "new"
+        assert a.old_path == "old"
+        assert a.backup_path == "new"
+        assert isinstance(a.app, MockApp)
+
+    def test_popup(self):
+        a = BackupAncienneDB("old", "new")
+        assert a.popup in self.window.children  # == opened
+
+    def test_popup_non(self):
+        a = BackupAncienneDB("old", "new")
+        a.app.stop.reset_mock()
+        self.popup_click("non")
+        assert a.app.stop.called == True
+
+    def test_popup_oui(self):
+        t = TempFile()
+        new = t.file.parent / "new"
+        a = BackupAncienneDB(t.file, new)
+        a.app.stop.reset_mock()
+        self.popup_click("oui")
+        assert new.is_file()
+        assert a.app.config.write.called
+        assert a.app._reload_app.called
